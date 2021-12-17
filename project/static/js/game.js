a968abc95d575b9b85f7eda20748ba001dc39c95
Alexander Mertens <alexander.mertens@student.thomasmore.be>
2021-12-17 15:16:24 +0100

    Import js file

--- project/static/js/game.js
+++ project/static/js/game.js
@@ -1,2 +1,13 @@
-export function startGame() {
+async function startGame() {
+    fetch('/start-game')
+    .then((response) => response.json()) 
+    .then(runGame)
+    .catch((err) => console.log(err));
 }
+
+function runGame(gameData) {
+    index = 0;
+    console.log(gameData)
+}
+
+startGame()
